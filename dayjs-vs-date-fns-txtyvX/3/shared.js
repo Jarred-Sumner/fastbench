5d1f0119b0dc83f439480f746566c458b96d951d
Fastbench <example@example.com>
2021-01-19 02:26:00 -0800

    New benchmark

---
+++ dayjs-vs-date-fns-txtyvX/3/shared.js
@@ -0,0 +1,4 @@
+import React from "https://cdn.skypack.dev/react@17.0.1";
+import dayjs from "https://cdn.skypack.dev/dayjs@1.10.3";
+import dateFns from "https://cdn.skypack.dev/date-fns@2.16.1";
+
