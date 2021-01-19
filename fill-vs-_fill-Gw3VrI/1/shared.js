991d0973138a042327cfe30476896089a5b6dd41
Fastbench <example@example.com>
2021-01-18 23:54:21 -0800

    New benchmark

---
+++ fill-vs-_fill-Gw3VrI/1/shared.js
@@ -0,0 +1,5 @@
+import _ from "https://cdn.skypack.dev/lodash";
+import {fill, sample} from "https://cdn.skypack.dev/lodash";
+import * as lodash from "https://cdn.skypack.dev/lodash";var a = new Array(100)
+
+var b = new Array();
