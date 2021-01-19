866883532fde506e9fef4621a5ab251c5d225585
Fastbench <example@example.com>
2021-01-19 02:00:31 -0800

    New benchmark

---
+++ fill-vs-_fill-aRPq-_/0/shared.js
@@ -0,0 +1,5 @@
+import React from "https://cdn.skypack.dev/react@17.0.1";
+import _ from "https://cdn.skypack.dev/lodash";
+import {fill, sample} from "https://cdn.skypack.dev/lodash";
+import * as lodash from "https://cdn.skypack.dev/lodash";var a = new Array(100)
+var b = new Array();
