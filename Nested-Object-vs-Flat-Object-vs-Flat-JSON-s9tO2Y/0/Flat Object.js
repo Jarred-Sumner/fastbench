ef4b1d20c8f07e997715e867209535185c07e653
Fastbench <example@example.com>
2021-05-19 21:45:38 -0700

    New benchmark

---
+++ Nested-Object-vs-Flat-Object-vs-Flat-JSON-s9tO2Y/0/Flat Object.js
@@ -0,0 +1,10 @@
+
+for (let i = 0; i < 1000; i++) {
+    for (let j = 0; j < 1000; j++) {
+        let id1 = '@user_' + i
+        let id2 = '@labsproject_' + j
+        let key = id1 + id2
+        let val = cache[key] || 0
+        cache[key] = val + 1
+    }
+}
