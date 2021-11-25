6b0c118704b9889d622bb4f14d2158b79b62abae
Fastbench <example@example.com>
2021-11-25 08:14:55 -0800

    New benchmark

---
+++ intersectRec-vs-intersect(re-nKe2yU/0/shared.js
@@ -0,0 +1,34 @@
+const rectA = {
+  left:   10,
+  top:    10,
+  right:  30,
+  bottom: 30
+};
+
+const rectB = {
+  left:   20,
+  top:    20,
+  right:  50,
+  bottom: 50
+};
+
+const rectC = {
+  left:   70,
+  top:    70,
+  right:  90,
+  bottom: 90
+};
+
+function intersectRect(r1, r2) {
+  return !(r2.left > r1.right || 
+           r2.right < r1.left || 
+           r2.top > r1.bottom ||
+           r2.bottom < r1.top);
+}
+
+function intersect(a, b) {
+  return (a.left <= b.right &&
+          b.left <= a.right &&
+          a.top <= b.bottom &&
+          b.top <= a.bottom)
+}
