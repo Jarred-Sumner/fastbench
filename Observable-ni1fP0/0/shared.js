5367c4a25bc7d8ff2ee069e1eaded5d9eb7351fa
Fastbench <example@example.com>
2021-12-07 01:06:10 -0800

    New benchmark

---
+++ Observable-ni1fP0/0/shared.js
@@ -0,0 +1,5 @@
+import iterOps from "https://cdn.skypack.dev/iter-ops@0.7.0";
+import * as rxjs from "https://cdn.skypack.dev/rxjs@7.3.0";
+import * as dxjs from "https://cdn.skypack.dev/dxjs@0.0.2";
+
+const array = new Array(200).fill(null).map((_, i) => i);
