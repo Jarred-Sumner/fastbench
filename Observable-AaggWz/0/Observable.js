1e35bd8fd35f348148d7df8a8031d7cf97fef2e2
Fastbench <example@example.com>
2021-12-07 01:05:00 -0800

    New benchmark

---
+++ Observable-AaggWz/0/Observable.js
@@ -0,0 +1,17 @@
+const subject = new rxjs.BehaviorSubject(0)
+
+const pipedSubject = subject.pipe(
+  rxjs.filter(a => a % 2 === 0),
+  rxjs.map(b => ({value: b})),
+);
+
+pipedSubject.subscribe((value) => {
+  // value
+});
+
+// Trigger value changes
+for (const i of [0, 1, 2, 3, 4]) {
+  subject.next(i);
+}
+
+subject.complete();
