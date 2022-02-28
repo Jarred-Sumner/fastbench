82c3aaa920eeaed375a322196dd5ca7b3f3543f9
Fastbench <example@example.com>
2022-02-27 22:31:33 -0800

    New benchmark

---
+++ regex-start-vs-startsWith-vs-indexOf-start-QC0VbH/0/charAt(0).js
@@ -0,0 +1,9 @@
+var x = 0;
+var TOTAL_STRINGS = TOTAL_STRINGS;
+var data = data;
+var regex = regexStart;
+while (x < TOTAL_STRINGS) {
+  const str = data[x];
+  str.charAt(0) === str;
+  x += 1;
+}
