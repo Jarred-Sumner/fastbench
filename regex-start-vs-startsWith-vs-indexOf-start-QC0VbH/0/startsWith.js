75725edb781914a2fe43c38daff5c15e6af463ad
Fastbench <example@example.com>
2022-02-27 22:31:32 -0800

    New benchmark

---
+++ regex-start-vs-startsWith-vs-indexOf-start-QC0VbH/0/startsWith.js
@@ -0,0 +1,9 @@
+var x = 0;
+var TOTAL_STRINGS = TOTAL_STRINGS;
+var data = data;
+var regex = regexStart;
+while (x < TOTAL_STRINGS) {
+  const str = data[x];
+  str.startsWith(match) === 0;
+  x += 1;
+}
