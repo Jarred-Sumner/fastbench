fd9b65f9fb994fab0586fce4b24e99d29c5e78f1
Fastbench <example@example.com>
2022-07-28 05:51:23 -0700

    New benchmark

---
+++ 3x-loop-vs-pre-format-map-AL0EcZ/0/shared.js
@@ -0,0 +1,34 @@
+const partner = [
+  {id: 0, languageRegion: [['deu', 'de'], ['eng', 'en']]},
+  {id: 1, languageRegion: [['deu', 'de']]},
+  {id: 2, languageRegion: [['eng', 'en']]},
+  {id: 3, languageRegion: [['deu', 'de'], ['eng', 'en']]},
+  {id: 4, languageRegion: [['deu', 'de'], ['eng', 'en']]},
+  {id: 5, languageRegion: [['eng', 'en']]},
+  {id: 6, languageRegion: [['eng', 'en']]},
+  {id: 7, languageRegion: [['deu', 'de']]},
+  {id: 8, languageRegion: [['deu', 'de']]},
+  {id: 9, languageRegion: [['deu', 'de'], ['eng', 'en']]},
+];
+
+const trigger = [
+  {id: 0, language: "deu"},
+  {id: 1, language: "deu"},
+  {id: 2, language: "deu"},
+  {id: 3, language: "eng"},
+  {id: 4, language: "eng"},
+  {id: 5, language: "deu"},
+  {id: 6, language: "eng"},
+  {id: 7, language: "deu"},
+  {id: 8, language: "deu"},
+  {id: 9, language: "deu"},
+];
+
+const triggerByLng = new Map();
+for (const t of trigger) {
+  if (triggerByLng.has(t.language)) {
+    triggerByLng.set(t.language, [...triggerByLng.get(t.language), t])
+  } else {
+    triggerByLng.set(t.language, [t])
+  }
+}
