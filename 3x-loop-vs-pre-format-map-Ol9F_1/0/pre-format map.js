59469a5c6f7f598ca89e4bb1bffa17909ac9159b
Fastbench <example@example.com>
2022-07-28 05:50:25 -0700

    New benchmark

---
+++ 3x-loop-vs-pre-format-map-Ol9F_1/0/pre-format map.js
@@ -0,0 +1,25 @@
+let queries = [];
+
+const triggerByLng = new Map();
+for (const t of trigger) {
+  if (triggerByLng.has(t.language)) {
+    triggerByLng.set(t.language, [...triggerByLng.get(t.language), t])
+  } else {
+    triggerByLng.set(t.language, [t])
+  }
+}
+
+for (const p of partner) {
+  const { languageRegion } = p;
+
+  for (const lr of p.languageRegion) {
+    const [language] = lr;
+    const tList = triggerByLng.get(language);
+    
+    for (const t of tList) {
+      if (t.language == language) {
+        queries.push({p, t})
+      } 
+    }
+  }
+}
