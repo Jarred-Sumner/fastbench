05c5013e4926a369c0a2aaa634939dedd7c80117
Fastbench <example@example.com>
2022-08-27 10:44:52 -0700

    New benchmark

---
+++ Format-"Wow"-JtYOF/0/shared.js
@@ -0,0 +1,5 @@
+import MessageFormat from "https://cdn.skypack.dev/@messageformat/core@3.0.1";
+import { MessagePipe } from "https://cdn.skypack.dev/messagepipe@0.2.1";
+
+const message = 'Wow';
+const props = {};
