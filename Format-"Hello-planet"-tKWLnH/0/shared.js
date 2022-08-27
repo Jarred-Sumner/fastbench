c9fc97b6931ad671621ff63ec3ad6ca5abe4c977
Fastbench <example@example.com>
2022-08-27 07:11:03 -0700

    New benchmark

---
+++ Format-"Hello-planet"-tKWLnH/0/shared.js
@@ -0,0 +1,7 @@
+import MessageFormat from "https://cdn.skypack.dev/@messageformat/core@3.0.1";
+import { MessagePipe } from "https://cdn.skypack.dev/messagepipe@0.2.1";
+
+const message = 'Hello {planet}!';
+const props = {
+  planet: "Mars",
+};
