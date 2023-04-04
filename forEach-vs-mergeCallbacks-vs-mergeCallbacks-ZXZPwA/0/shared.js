d06437ff6aa3da99e9624aa8bf446eaedd0a68b3
Fastbench <example@example.com>
2023-04-04 00:44:02 -0700

    New benchmark

---
+++ forEach-vs-mergeCallbacks-vs-mergeCallbacks-ZXZPwA/0/shared.js
@@ -0,0 +1,25 @@
+function mergeCallbacks(arr) {
+  const merger = eval(`(function (collection) {
+    ${arr.map((_, i) => `var f${i} = collection[${i}];`).join(" ")}
+    return (function () {
+      ${arr.map((_, i) => `f${i}();`).join(" ")}
+    });
+  })`);
+  const baked = merger(arr);
+  return baked;
+}
+
+function mergeCallbacks2(arr) {
+  const merger = new Function('collection', `
+    ${arr.map((_, i) => `var f${i} = collection[${i}];`).join(" ")}
+    return (function () {
+      ${arr.map((_, i) => `f${i}();`).join(" ")}
+    });`);
+  const baked = merger(arr);
+  return baked;
+}
+
+const callbacks = [() => 1, () => 2, () => 3, () => 4];
+
+const mergedCallbacks = mergeCallbacks(callbacks)
+const mergedCallbacks2 = mergeCallbacks2(callbacks)
