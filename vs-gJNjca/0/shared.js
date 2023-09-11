2cbbf21a8f06de8b65ecca8dc751637feda179c9
Fastbench <example@example.com>
2023-09-11 09:31:49 -0700

    New benchmark

---
+++ vs-gJNjca/0/shared.js
@@ -0,0 +1,14 @@
+function random() {
+  const num = Math.random();
+  if (num > 0.3) return num + '';
+  return num;
+}
+
+function num () {
+  return Math.random()
+}
+
+function run (a, b) {
+  return a;
+}
+
