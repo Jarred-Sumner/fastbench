33442af8a8f42663768a93f9dfeff60dc9489c54
Fastbench <example@example.com>
2023-09-12 12:34:54 -0700

    New benchmark

---
+++ function-map-vs-Arrayprotot-XccT1t/0/Arrayprotot.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+function mapArray(array, callback) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(callback(array[i], i, array));
+    }
+    return result;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+let squaredNumbers = mapArray(numbers, (number) => {return number * number;})
+//console.log(squaredNumbers); // [1, 4, 9, 16, 25]
