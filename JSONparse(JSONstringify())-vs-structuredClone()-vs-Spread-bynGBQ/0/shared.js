fb8c0124a49d2f1cea1b862e31a580520a651e86
Fastbench <example@example.com>
2023-10-18 13:46:53 -0700

    New benchmark

---
+++ JSONparse(JSONstringify())-vs-structuredClone()-vs-Spread-bynGBQ/0/shared.js
@@ -0,0 +1,5 @@
+const array = [
+  { id: crypto.randomUUID(), value: Math.random(), targets: ["1164302916006187008", "1164302950227513344"], type: "Abc" },  
+  { id: crypto.randomUUID(), value: Math.random(), targets: ["1164302980019654656"], type: "Def" },
+  { id: crypto.randomUUID(), value: Math.random(), targets: [], type: "Ghi" },
+];
