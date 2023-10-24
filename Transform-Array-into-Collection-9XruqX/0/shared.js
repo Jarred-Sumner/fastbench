0037991df78b59cefb817958366b545bd671a4ba
Fastbench <example@example.com>
2023-10-24 09:49:34 -0700

    New benchmark

---
+++ Transform-Array-into-Collection-9XruqX/0/shared.js
@@ -0,0 +1,21 @@
+import { Collection } from "@discordjs/collection";
+
+const data = [
+  { id: "123123123", name: "abcd" },
+  { id: "123412341", name: "yeet" },
+  { id: "121212121", name: "yote" },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+  { id: "111111111", name: "etc." },
+];
