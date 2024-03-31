c288c77c23cc35a08dc675c847e48e85485f116c
Fastbench <example@example.com>
2024-03-31 11:20:52 -0700

    New benchmark

---
+++ Array-JSONstringify(x)-vs-xtoString()-0prx0s/0/shared.js
@@ -0,0 +1,6 @@
+const array = [
+  { key1: "1231453549823489", key2: 10, key3: "asodjidsi", nested: { obj1: "12893893891", key44: "asoidiadsioasd"  } },
+  { key1: "1231453549823", key2: 103, key3: "asodjid", nested: { obj1: "128938912", key44: "adiadsioasd"  } },
+  { key1: "1231453549823489", key2: 0, key3: "asoidsi", nested: { obj1: "1289123891", key44: "asoidiadd"  } },
+  { key1: "123145389", key2: 59549, key3: "adjidsi", nested: { obj1: "1289389891", key44: "asoidsioasd"  } },
+];
