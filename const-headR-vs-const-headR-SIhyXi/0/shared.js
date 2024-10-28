36cf6e4d10f8bde5e62c267f7b27f9135abf21c7
Fastbench <example@example.com>
2024-10-28 06:18:31 -0700

    New benchmark

---
+++ const-headR-vs-const-headR-SIhyXi/0/shared.js
@@ -0,0 +1,5 @@
+const input = `
+| title        |  description   |     heading 1 | heading 2              |
+| :----------- | :------------: | ------------: | ---------------------- |
+| left-aligned | center-aligned | right-aligned | default text alignment |
+`
