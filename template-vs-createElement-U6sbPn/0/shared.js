b7bef13c2e626590ec268c19ea45278ad2b0cca0
Fastbench <example@example.com>
2025-10-20 03:22:23 -0700

    New benchmark

---
+++ template-vs-createElement-U6sbPn/0/shared.js
@@ -0,0 +1,10 @@
+function template(html) {
+  let node;
+  const create = () => {
+    const t = document.createElement('template')
+    t.innerHTML = html
+    return t.content.firstChild
+  }
+  return () => (node || (node = create())).cloneNode(true);
+}
+let h1Template;
