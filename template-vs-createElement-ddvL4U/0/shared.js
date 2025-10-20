f95964c2aee91b7d0a093af763eaff7e39e0c0b2
Fastbench <example@example.com>
2025-10-20 03:21:55 -0700

    New benchmark

---
+++ template-vs-createElement-ddvL4U/0/shared.js
@@ -0,0 +1,9 @@
+function template(html) {
+  let node;
+  const create = () => {
+    const t = document.createElement('template')
+    t.innerHTML = html
+    return t.content.firstChild
+  }
+  return () => (node || (node = create())).cloneNode(true);
+}
