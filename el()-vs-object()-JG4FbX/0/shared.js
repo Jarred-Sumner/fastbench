bd3cc5ba648f08884e6ed024e8794ff03a6c23c9
Fastbench <example@example.com>
2025-10-29 03:26:00 -0700

    New benchmark

---
+++ el()-vs-object()-JG4FbX/0/shared.js
@@ -0,0 +1,9 @@
+function string() {
+  const el = document.createElement("div");
+  el.style = "background-color:red"
+}
+
+function object() {
+  const el = document.createElement("div");
+  el.style.backgroundColor = "red"
+}
