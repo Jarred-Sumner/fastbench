8cd10d9ca262894be568789f1e3fe104853a68ac
Fastbench <example@example.com>
2025-10-29 03:29:28 -0700

    New benchmark

---
+++ el()-vs-object()-l2HEDI/0/shared.js
@@ -0,0 +1,20 @@
+function string() {
+  const el = document.createElement("div");
+  el.style = "background-color:red;color:orange;width:100px;"
+}
+
+function object() {
+  const el = document.createElement("div");
+  el.style.backgroundColor = "red";
+  el.style.color = "orange";
+  el.style.width = "100px";
+}
+
+function assign() {
+  const el = document.createElement("div");
+  Object.assign(el.style, {
+    backgroundColor: "red",
+    color: "orange",
+    width: "100px",
+  })
+}
