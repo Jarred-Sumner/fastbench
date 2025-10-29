7a1afd3335f82b72b683d7b4d6c7c750f04b7194
Fastbench <example@example.com>
2025-10-29 03:13:36 -0700

    New benchmark

---
+++ create()-vs-clone()-vs-template()-y43PlT/0/shared.js
@@ -0,0 +1,31 @@
+function create() {
+  const el = document.createElement("div")
+  el.style.backgroundColor = "red"
+  el.innerText = "test"
+  return el
+}
+
+const tmp = document.createElement("div")
+function clone() {
+  const el = tmp.cloneNode()
+  el.style.backgroundColor = "red"
+  el.innerText = "test"
+  return el
+}
+
+function t(html) {
+  let node;
+  const create = () => {
+    const t = document.createElement('template')
+    t.innerHTML = html
+    return t.content.firstChild
+  }
+  return () => (node || (node = create())).cloneNode(true);
+}
+
+function template() {
+  const el = t("<div style='background-color:red'>test</div>")
+  // el.style.backgroundColor = "red"
+  // el.innerText = "test"
+  return el
+}
